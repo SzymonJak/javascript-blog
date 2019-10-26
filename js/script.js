56d59229930ca5c31c4c379997fa306d71fd858e
Szymon Jakubiak <jakubiak.sz@gmail.com>
2019-10-26 13:13:46 +0200

    Install ESLint and check js syntax

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
         /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
 
-        for(let activeLink of activeLinks){
+        for (let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
         /* [DONE] add class 'active' to the clicked link */
@@ -16,7 +16,7 @@
         /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('article.active');
 
-        for (let activeArticle of activeArticles){
+        for (let activeArticle of activeArticles) {
             activeArticle.classList.remove('active');
         }
         /* [DONE] get 'href' attribute from the clicked link */
@@ -27,24 +27,24 @@
 
         /* [DONE] add class 'active' to the correct article */
         targetArticle.classList.add('active');
-    }
+    };
 
     const optArticleSelector = '.post',
         optTitleSelector = '.post-title',
         optTitleListSelector = '.titles';
 
-    const generateTitleLinks = function() {
+    const generateTitleLinks = function () {
         const titleList = document.querySelector(optTitleListSelector);
         /* remove content of links' list */
         titleList.innerHTML = '';
-       
+
         /* Find all articles */
         const articles = document.querySelectorAll(optArticleSelector);
 
         let html = '';
 
         for (let article of articles) {
-            
+
             /* get the article id */
             const articleId = article.getAttribute('id');
             /* Find title element and get title */
@@ -57,13 +57,13 @@
         }
         titleList.innerHTML = html;
         // titleList.insertAdjacentHTML('beforeend', html);
-        
+
         const links = document.querySelectorAll('.titles a');
-        
+
         for (let link of links) {
             link.addEventListener('click', titleClickHandler);
         }
-    }
+    };
 
     generateTitleLinks();
 
