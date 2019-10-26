a2d2ff7b4b9482e5c8a5f4bded8b3c07e42435bd
Szymon Jakubiak <jakubiak.sz@gmail.com>
2019-10-26 12:01:34 +0200

    Correct remove content element

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@
     const titleClickHandler = function (event) {
         event.preventDefault();
         const clickedElement = this;
-        console.log('Link was clicked!');
-        console.log(event);
+
         /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -14,9 +13,6 @@
         /* [DONE] add class 'active' to the clicked link */
         clickedElement.classList.add('active');
 
-        console.log('clickedElement: ', clickedElement);
-        console.log('clickedElement (with plus): ' + clickedElement);
-
         /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('article.active');
 
@@ -25,11 +21,9 @@
         }
         /* [DONE] get 'href' attribute from the clicked link */
         const articleSelector = clickedElement.getAttribute('href');
-        console.log(articleSelector);
 
         /* [DONE] find the correct article using the selector (value of 'href' attribute) */
         const targetArticle = document.querySelector(articleSelector);
-        console.log(targetArticle);
 
         /* [DONE] add class 'active' to the correct article */
         targetArticle.classList.add('active');
@@ -41,13 +35,9 @@
 
     const generateTitleLinks = function() {
         const titleList = document.querySelector(optTitleListSelector);
-        console.log('List will be generated');
         /* remove content of links' list */
-        let clearList = function() {
-            titleList.innerHTML = '';
-            console.log('clear!');
-        }
-        clearList();
+        titleList.innerHTML = '';
+       
         /* Find all articles */
         const articles = document.querySelectorAll(optArticleSelector);
 
@@ -64,7 +54,6 @@
             /* - insert created HTML code into html variable */
 
             html = html + linkHTML;
-            console.log(html);
         }
         titleList.innerHTML = html;
         // titleList.insertAdjacentHTML('beforeend', html);
